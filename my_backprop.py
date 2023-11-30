fe95969147394f3bf00174d8d8b237df59dbaca1
rob <robert.koehlmoos@gmail.com>
2023-11-29 21:09:18 -0500

    Everything working!

--- my_backprop.py
+++ my_backprop.py
@@ -1,8 +1,48 @@
 from keras.datasets import mnist
+import numpy as np
 
+import neural_network
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
 print(f'X_train: {train_X.shape}')
 print(f'Y_train: {train_y.shape}')
 print(f'X_test:  {test_X.shape}')
 print(f'Y_test:  {test_y.shape}')
+
+# Transforming the keras supplied mnist dataset to work for training the neural network
+train_x_flattened = [x.flatten().reshape((-1, 1)) for x in train_X]
+test_x_flattened = [x.flatten().reshape((-1, 1)) for x in test_X]
+train_y_vectorized = [np.zeros((10, 1)) for _ in train_y]
+for i, y in enumerate(train_y):
+    train_y_vectorized[i][y] = 1.0
+test_y_vectorized = [np.zeros((10, 1)) for _ in test_y]
+for i, y in enumerate(test_y):
+    test_y_vectorized[i][y] = 1.0
+
+my_nn = neural_network.MLP([28*28, 30, 30, 10])
+
+my_nn.sgd(train_x_flattened[:10000], train_y_vectorized[:10000], test_x_flattened, test_y_vectorized, epochs=20, batch_size=100)
+
+# example training run
+# Accuracy for epoch 0: 0.4613
+# Accuracy for epoch 1: 0.5337
+# Accuracy for epoch 2: 0.5739
+# Accuracy for epoch 3: 0.6145
+# Accuracy for epoch 4: 0.6371
+# Accuracy for epoch 5: 0.663
+# Accuracy for epoch 6: 0.6785
+# Accuracy for epoch 7: 0.6952
+# Accuracy for epoch 8: 0.7015
+# Accuracy for epoch 9: 0.7107
+# Accuracy for epoch 10: 0.7175
+# Accuracy for epoch 11: 0.7239
+# Accuracy for epoch 12: 0.7302
+# Accuracy for epoch 13: 0.7332
+# Accuracy for epoch 14: 0.7388
+# Accuracy for epoch 15: 0.7427
+# Accuracy for epoch 16: 0.7479
+# Accuracy for epoch 17: 0.7464
+# Accuracy for epoch 18: 0.7513
+# Accuracy for epoch 19: 0.7589
+#
+# Process finished with exit code 0
--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List, Optional
 
 import numpy as np
 
@@ -9,7 +9,7 @@ def logistic(x: np.ndarray) -> np.ndarray:
     :param x: an input array
     :return: An array of the same size
     """
-    return 1.0/(1.0+np.exp(-1*x))
+    return 1.0 / (1.0 + np.exp(-1 * x))
 
 
 def logistic_prime(x: np.ndarray) -> np.ndarray:
@@ -18,46 +18,96 @@ def logistic_prime(x: np.ndarray) -> np.ndarray:
     :param x: an input array
     :return: An array of the same size
     """
-    normal_logistic = 1.0/(1.0+np.exp(-1*x))
-    return normal_logistic * (1-normal_logistic)
+    normal_logistic = 1.0 / (1.0 + np.exp(-1 * x))  # efficiency
+    return normal_logistic * (1 - normal_logistic)
 
 
 class MLP:
-    def __init__(self, layer_sizes: Iterable[int]):
+    def __init__(self, layer_sizes: Iterable[int], seed: Optional[int] = None):
+        if seed:
+            np.random.seed(seed)  # good science is repeatable
+        self.layer_sizes = layer_sizes
         self.biases = [np.random.randn(y, 1) for y in layer_sizes[1:]]
+        # x is the size of the output from the previous layer, y is the size of the layer the weight outputs are going to
         self.weights = [np.random.randn(y, x) for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
 
     def feedforward(self, x: np.ndarray) -> np.ndarray:
         """
         Performs a feedforward run of the network and returns the output
-        :param x: An array of equal size to the first layer in the MLP
-        :return: An array of equial size to the last layer in the MLP
+        :param x: An array of equal size to the first layer in the MLP, with shape (n, 1)
+        :return: An array of equal size to the last layer in the MLP, with shape (m, 1)
         """
         activation = x
         for weight, bias in zip(self.weights, self.biases):
             activation = logistic((weight @ activation) + bias)
         return activation
 
-    def backprop(self, x: np.ndarray, y: np.ndarray) -> (np.ndarray, np.ndarray):
+    def sgd(self, train_x: List[np.ndarray], train_y: List[np.ndarray], test_x: List[np.ndarray],
+            test_y: List[np.ndarray], epochs: int, batch_size: int, learning_rate: float = 0.2) -> None:
         """
-        Runs backprop for a single example thorugh the network
-        :param x: An input
-        :param y: A target output of the value
+        Performs stochastic gradient descent using training data, while
+        :param train_x: Training inputs
+        :param train_y: Training target
+        :param test_x: Testing inputs
+        :param test_y: Testing targets
+        :param epochs: The number of training runs to perform
+        :param batch_size: Size of each training batch before update
+        :param learning_rate: Learning rate to use when updating the model's parameters
+        """
+        for i in range(epochs):
+            # training
+            batches = (zip(train_x[j: j + batch_size], train_y[j: j + batch_size])
+                       for j in range(0, len(train_x), batch_size))
+            for batch in batches:
+                # creating 0 filled matrices to accumulate the rates of change
+                weights_update = [np.zeros(weight.shape) for weight in self.weights]
+                biases_update = [np.zeros(bias.shape) for bias in self.biases]
+                for x, y in batch:
+                    delta_weights, delta_biases = self.backprop(x, y)
+                    weights_update = [weight_update + delta_weight
+                                      for weight_update, delta_weight in zip(weights_update, delta_weights)]
+                    biases_update = [bias_update + delta_bias
+                                     for bias_update, delta_bias in zip(biases_update, delta_biases)]
+                # performing the updates
+                self.weights = [weight - (learning_rate / batch_size) * weight_update
+                                for weight, weight_update in zip(self.weights, weights_update)]
+                self.biases = [bias - (learning_rate / batch_size) * bias_update
+                               for bias, bias_update in zip(self.biases, biases_update)]
+            # running tests to track performance based on accuracy
+            correct = 0
+            for x, y in zip(test_x, test_y):
+                prediction = self.feedforward(x)
+                if np.argmax(prediction) == np.argmax(y):
+                    correct += 1
+            print(f"Accuracy for epoch {i}: {correct / len(train_x)}")
+
+    def backprop(self, x: np.ndarray, y: np.ndarray) -> (List[np.ndarray], List[np.ndarray]):
+        """
+        Runs backprop for a single example through the network
+        :param x: An input with shape (n, 1) where n is the size of the first layer
+        :param y: A target output of the input with shape (m, 1), where m is the size of the last layer
         :return: The weight and bias rates of change with respect to the cost
         """
         activations = [x]
         zs = []
         delta_weights = []
         delta_biases = []
+        # feedforward to collect activations as the neuron sums (zs)
         for weight, bias in zip(self.weights, self.biases):
             zs.append((weight @ activations[-1]) + bias)
-            activations.append(logistic(activations[-1]))
-        delta_a = activations[-1] - y
-        for i in range(1, len(self.weights) - 1):
-            delta_z = delta_a * logistic_prime(activations[-i])
-            delta_biases = delta_z
-            delta_weights = np.outer(delta_z @ activations[-i - 1])
-            delta_a = self.weights[-i].T @ delta_z
+            activations.append(logistic(zs[-1]))
+        # backprop step
+        delta_a = activations[-1] - y  # taking the derivative of the cost for each activation
+        delta_z = delta_a * logistic_prime(activations[-1])  # back propagating from the last output to the last sum
+        delta_biases.append(delta_z)  # the differentials for biases are equal to the delta for the sum
+        delta_weights.append(delta_z @ activations[-2].T)  # calculating differentials for weights
+        # continuing to backpropagation through each layer
+        for i in range(2, len(self.weights) + 1):
+            delta_a = self.weights[
+                          -i + 1].T @ delta_z  # backpropagation from the layer's sum to the previous layer's activations
+            delta_z = delta_a * logistic_prime(activations[-i])  # backpropagation from the activation to the sum
+            delta_biases.append(delta_z)
+            delta_weights.append(delta_z @ activations[-i - 1].T)
         delta_weights.reverse()
         delta_biases.reverse()
         return delta_weights, delta_biases
